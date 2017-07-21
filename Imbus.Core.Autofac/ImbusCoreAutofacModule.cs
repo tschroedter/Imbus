8792ed76bd90c8122804091ff07fdc30dbe4c419
thomas <Thomas.Schroedter@gmx.net>
2017-07-21 19:08:33 +1000

    Move files from private repo.

---
+++ Imbus.Core.Autofac/ImbusCoreAutofacModule.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Autofac;
+using Imbus.Core.Interfaces;
+using JetBrains.Annotations;
+
+namespace Imbus.Core.Autofac
+{
+    [UsedImplicitly]
+    [ExcludeFromCodeCoverage]
+    public class ImbusCoreAutofacModule
+        : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType <InMemoryMessageBus>()
+                   .As <IMessageBus>()
+                   .SingleInstance();
+            builder.RegisterType <MessageAggregator>()
+                   .As <IMessageAggregator>();
+            builder.RegisterType <PadlockStore>()
+                   .As <IPadlockStore>();
+            builder.RegisterType <SubscriberRepository>()
+                   .As <ISubscriberRepository>();
+            builder.RegisterType <SubscriberStore>()
+                   .As <ISubscriberStore>();
+        }
+    }
+}
